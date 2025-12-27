a04698e56a1a070f2eabdd7f4abb4e8430701e4e
Pedrocode-master <149103039+Pedrocode-master@users.noreply.github.com>
2025-12-27 11:57:58 -0300

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,8 +0,0 @@
-# utils/__init__.py
-"""
-Módulo de utilitários (mantém utils.py original + adiciona route_optimizer)
-"""
-
-from .route_optimizer import RouteOptimizer
-
-__all__ = ['RouteOptimizer']
